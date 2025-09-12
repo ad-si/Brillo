b6b1be72e185bd2e057f13b461c5770677772780
Adrian Sieber <mail@adriansieber.com>
2025-09-12 21:17:58 +0000

    Upgrade Stackage to lts-23.28

--- flake.nix
+++ flake.nix
@@ -21,8 +21,13 @@
             bash
             coreutils
             gnumake
-            haskellPackages.cabal-install
             haskell.compiler.ghc98
+            haskellPackages.cabal-fmt
+            haskellPackages.cabal-install
+            haskellPackages.fourmolu
+            haskellPackages.haskell-language-server
+            haskellPackages.hlint
+            haskellPackages.stack
           ];
         };
         formatter = pkgs.nixfmt-tree; # Format this file with `nix fmt`
